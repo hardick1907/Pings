7fed94125a4333ee4ba6b19c5770af2c9170d7b2
hardick <bhadauriahardick@gmail.com>
2024-11-30 01:27:48 +0530

    backend update for deploy

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,9 +21,9 @@ app.use('/api/user',userRoutes);
 app.use('/api/room',roomRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.use(express.static(path.join(__dirname,'../Frontend/dist')));
     app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
     })
 }
 
