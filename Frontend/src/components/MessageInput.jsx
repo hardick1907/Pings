38056fa8ca2b98aa75b10c9663b1ae0174dd18c1
hardick <bhadauriahardick@gmail.com>
2024-11-30 18:16:53 +0530

    image button for mobile added

--- Frontend/src/components/MessageInput.jsx
+++ Frontend/src/components/MessageInput.jsx
@@ -5,7 +5,6 @@ import toast from "react-hot-toast";
 import { useRoomStore } from "../store/useRoomStore.js";
 
 export default function MessageInput({ roomId }) {
-  console.log("Room ID:", roomId);
   const { sendMessageToRoom } = useRoomStore();
   const [editor, setEditor] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
@@ -43,7 +42,7 @@ export default function MessageInput({ roomId }) {
       await sendMessageToRoom(roomId, messageData);
 
       if (editor) {
-      editor.commands.clearContent();
+        editor.commands.clearContent();
       }
       setImagePreview(null);
     } catch (error) {
@@ -53,34 +52,58 @@ export default function MessageInput({ roomId }) {
   };
 
   return (
-    <div className="flex items-center p-2 rounded-lg">
+    <div className="flex flex-col">
       {imagePreview && (
-      <div className="mb-3 flex items-center gap-2">
-        <div className="relative">
-          <img src={imagePreview} alt="Preview" className="w-20 h-20 object-cover rounded-lg border border-zinc-700" />
-          <button onClick={removeImage}
-            className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
-            type="button">
-            <X className="size-3" />
-          </button>
+        <div className="mb-2 flex items-center gap-2">
+          <div className="relative">
+            <img 
+              src={imagePreview} 
+              alt="Preview" 
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700" 
+            />
+            <button 
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
         </div>
-      </div>
       )}
 
-      <div className="flex-grow">
-        <TipTap onEditorReady={(editorInstance)=> setEditor(editorInstance)} onSendMessage={handleSendMessage} />
+      <div className="flex items-center space-x-2">
+        <div className="flex-grow">
+          <TipTap 
+            onEditorReady={(editorInstance) => setEditor(editorInstance)} 
+            onSendMessage={handleSendMessage} 
+          />
+        </div>
+
+        <button 
+          type="button" 
+          className="btn btn-circle" 
+          onClick={handleSendMessage}
+        >
+          <Send size={24} />
+        </button>
       </div>
 
-      <div className="flex space-x-2 ml-0">
-        <input type="file" accept="image/*" className="hidden" ref={fileInputRef} onChange={handleImageChange} />
-        <button type="button" className={`hidden sm:flex btn btn-outline ${imagePreview ? 'btn-active' : '' }`}
-          onClick={()=> fileInputRef.current.click()}
-          >
-          <Image size={16} />
-        </button>
-
-        <button type="button" className="btn btn-circle" onClick={handleSendMessage}>
-          <Send size={24} />
+      <div className="flex justify-center items-center mt-2 space-x-2">
+        <input 
+          type="file" 
+          accept="image/*" 
+          className="hidden" 
+          ref={fileInputRef} 
+          onChange={handleImageChange} 
+        />
+        <button 
+          type="button" 
+          className="btn btn-outline w-half"
+          onClick={() => fileInputRef.current.click()}
+        >
+          <Image size={16} className="mr-2" />
+          Add Image
         </button>
       </div>
     </div>
